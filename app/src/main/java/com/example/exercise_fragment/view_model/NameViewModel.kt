414c97c3efaad36bc0e6720312d31b2376f6960f
Muhammad Yusril Yuriis <61834564+myyuriis@users.noreply.github.com>
2020-08-14 16:33:26 +0700

    add new live data

---
+++ app/src/main/java/com/example/exercise_fragment/view_model/NameViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.exercise_fragment.view_model
+
+//import androidx.lifecycle.MutableLiveData
+//import androidx.lifecycle.ViewModel
+//
+//class NameViewModel: ViewModel() {
+//
+//    val playerName: MutableLiveData<String> = MutableLiveData("")
+//
+//    fun changeName(name: String) {
+//        playerName.value = name
+//    }
+//}
